8857cc77f93465b60a937dee800d5634b3e0f9ba
Cem Saldırım <cem.saldirim@gmail.com>
2023-08-28 02:32:52 +0300

    LL peek peek_mut

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -1,45 +1,54 @@
 use std::mem;
 
+type Link<T> = Option<Box<Node<T>>>;
+
 struct Node<T> {
     item: T,
-    next: Option<Box<Node<T>>>,
+    next: Link<T>,
 }
 
 pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
+    head: Link<T>,
 }
 
 impl<T> LinkedList<T> {
-    pub fn new() -> LinkedList<T> {
+    pub fn new() -> Self {
         LinkedList { head: None }
     }
 
     pub fn push(&mut self, item: T) {
         let new_node = Box::new(Node {
             item,
-            next: mem::replace(&mut self.head, None),
+            next: mem::take(&mut self.head),
         });
         self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        let result;
-        match mem::replace(&mut self.head, None) {
-            Some(e) => {
-                result = Some(e.item);
-                self.head = e.next;
-            }
-            None => result = None,
-        }
-        result
+        mem::take(&mut self.head).map(|node| {
+            self.head = node.next;
+            node.item
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.item
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.item
+        })
     }
 }
 
 impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
-        let mut link = mem::replace(&mut self.head, None);
+        let mut link = mem::take(&mut self.head);
         while let Some(mut node) = link {
-            link = mem::replace(&mut node.next, None);
+            link = mem::take(&mut node.next);
         }
     }
 }
@@ -54,5 +63,15 @@ mod tests {
         (0..10).for_each(|n| list.push(n));
         (0..10).rev().for_each(|n| assert_eq!(list.pop(), Some(n)));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = LinkedList::new();
+        list.push(1);
+        list.push(2);
+        assert_eq!(list.peek(), Some(&2));
+        list.peek_mut().map(|item| *item = 3);
+        assert_eq!(list.peek(), Some(&3));
     }
 }
